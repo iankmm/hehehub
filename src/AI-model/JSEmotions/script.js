ac90d554d43305355cdfd8a4f93e53f30749174b
송경화 <sgtsong@song-gyeonghwaui-noteubug.local>
2025-02-27 22:26:41 -0700

    Initial commit on new-facial-recognition

--- src/AI-model/JSEmotions/script.js
+++ src/AI-model/JSEmotions/script.js
@@ -10,14 +10,6 @@ Promise.all([
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo)
 
-// function startVideo() {
-//   navigator.getUserMedia(
-//     { video: {} },
-//     stream => video.srcObject = stream,
-//     err => console.error(err)
-//   )
-// }
-
 function startVideo() {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(stream => {
@@ -55,20 +47,3 @@ captureBtn.addEventListener('click', async () => {
   link.textContent = "Download Screenshot";
   document.body.appendChild(link);
 });
-
-
-// video.addEventListener('play', () => {
-//   const canvas = faceapi.createCanvasFromMedia(video)
-//   document.body.append(canvas)
-//   const displaySize = { width: video.width, height: video.height }
-//   faceapi.matchDimensions(canvas, displaySize)
-//   setInterval(async () => {
-//     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-//     const resizedDetections = faceapi.resizeResults(detections, displaySize)
-//     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-//     faceapi.draw.drawDetections(canvas, resizedDetections)
-//     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-//     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-    
-//   }, 100)
-// })
