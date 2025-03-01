d90062c3aa1a10954b9518ca27f34fd5e6ab72cd
Surya Sunkari <suryasunkari@gmail.com>
2025-03-01 08:09:52 -0700

    added comments

--- src/lib/chains.ts
+++ src/lib/chains.ts
@@ -4,7 +4,7 @@ import { zkSyncSepolia } from "thirdweb/chains";
 
 const flow = defineChain(545);
 const ink = defineChain(763373);
-// TODO: const u2u = defineChain(2484);
+const u2u = defineChain(2484);
 const zircuit = defineChain(48899);
 
 
@@ -13,5 +13,5 @@ const zircuit = defineChain(48899);
 // export const selectedChain = zkSyncSepolia;
 // export const selectedChain = flow;
 // export const selectedChain = ink;
-// TODO: export const selectedChain = u2u;
-export const selectedChain = zircuit;
+export const selectedChain = u2u;
+// export const selectedChain = zircuit;
